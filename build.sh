57996cfef5170b984e8f4512a1f8be1720c586d4
Ava Gordon <avagordon01@gmail.com>
2024-11-02 14:41:31 +0000

    add machinectl export step to build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ set -ex
 source .env
 echo $CR_PAT | sudo podman login ghcr.io -u avagordon01 --password-stdin
 for cf in containers/*.Containerfile; do
+    #build and tag
     name=$(basename "${cf}" .Containerfile)
     version=$(git describe --always --dirty)
     sudo podman build \
@@ -13,7 +14,14 @@ for cf in containers/*.Containerfile; do
         .
     sudo podman tag \
         "${name}":latest "${name}":"${version}"
+    #push to ghcr
     sudo podman push "${name}":latest ghcr.io/avagordon01/"${name}":latest
     sudo podman push "${name}":"${version}" ghcr.io/avagordon01/"${name}":"${version}"
+    #export to machinectl
+    sudo podman save -o "${name}".tar localhost/base:latest
+    sudo machinectl remove "${name}"
+    sudo importctl import-tar --force --class=machine "${name}".tar
+    rm -f "${name}".tar
 done
 sudo podman image list
+machinectl list-images
