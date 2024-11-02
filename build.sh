ed06cdc2306c197069edc60d6931fc9281336c3b
Ava Gordon <avagordon01@gmail.com>
2024-11-02 13:29:06 +0000

    add podman push step

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+source .env
+echo $CR_PAT | sudo podman login ghcr.io -u avagordon01 --password-stdin
 for cf in containers/*.Containerfile; do
     name=$(basename "${cf}" .Containerfile)
     version=$(git describe --always --dirty)
@@ -11,5 +13,7 @@ for cf in containers/*.Containerfile; do
         .
     sudo podman tag \
         "${name}":latest "${name}":"${version}"
+    sudo podman push "${name}":latest ghcr.io/avagordon01/"${name}":latest
+    sudo podman push "${name}":"${version}" ghcr.io/avagordon01/"${name}":"${version}"
 done
 sudo podman image list
