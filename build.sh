2061ae3075a74cb01f130c0bb053a7c45de76dc9
Ava Gordon <avagordon01@gmail.com>
2024-11-24 17:10:58 +0000

    build.sh push to gcp container registry

--- build.sh
+++ build.sh
@@ -2,10 +2,8 @@
 
 set -ex
 
-source .env
-echo $CR_PAT | sudo podman login ghcr.io -u avagordon01 --password-stdin
+#build and tag
 for cf in containers/*.Containerfile; do
-    #build and tag
     name=$(basename "${cf}" .Containerfile)
     version=$(git describe --always --dirty)
     name_l="${name}:latest"
@@ -15,7 +13,24 @@ for cf in containers/*.Containerfile; do
         -t "${name_l}" \
         -t "${name_v}" \
         .
-    #push to ghcr
-    sudo podman push "${name_l}" "ghcr.io/avagordon01/${name_l}"
-    sudo podman push "${name_v}" "ghcr.io/avagordon01/${name_v}"
+done
+
+#login
+source .env
+echo $CR_PAT | sudo podman login ghcr.io -u avagordon01 --password-stdin
+gh_cr="ghcr.io/avagordon01"
+GCP_AT=$(gcloud auth print-access-token)
+echo $GCP_AT | sudo podman login -u oauth2accesstoken --password-stdin us-east1-docker.pkg.dev
+gcp_cr="us-east1-docker.pkg.dev/golden-imprint-442423-f2/cr"
+
+#push to registries
+for cf in containers/*.Containerfile; do
+    name=$(basename "${cf}" .Containerfile)
+    version=$(git describe --always --dirty)
+    name_l="${name}:latest"
+    name_v="${name}:${version}"
+    sudo podman push "${name_l}" "${gh_cr}/${name_l}"
+    sudo podman push "${name_v}" "${gh_cr}/${name_v}"
+    sudo podman push "${name_l}" "${gcp_cr}/${name_l}"
+    sudo podman push "${name_v}" "${gcp_cr}/${name_v}"
 done
