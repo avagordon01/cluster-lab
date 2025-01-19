c3412795257ef89af65349f8ad95b3d7ebf3fd80
Ava Gordon <avagordon01@gmail.com>
2025-01-19 05:19:40 +0000

    add skeleton python import

---
+++ platform/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import pandas as pd
+import ibis
+
+import plotly.express as px
+
+import yfinance as yf
+
+# import catboost
+import statsmodels
+import sklearn
+import scipy
+
+print('hello')
